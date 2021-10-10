6ea43963c9a8525a084be4b354f13ce7a0e6dd28
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-10-11 01:48:04 +0600

    Project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Styles/App.css";
+import "./Styles/My_Classes.css";
+function App() {
+  return (
+    <div className="App-header bg-this-primary">
+      <h1 className="this-semi-dark">Hi react.js</h1>
+    </div>
+  );
+}
+export default App;
